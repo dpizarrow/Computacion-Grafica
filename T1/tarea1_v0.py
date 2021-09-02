d8e40075e9f40f4a231f1a9818dd88aa16e2d5ad
Diego <diego.pizarro0@gmail.com>
2021-09-01 21:25:21 -0400

    Agregar funcion para crear tablero

--- T1/tarea1_v0.py
+++ T1/tarea1_v0.py
@@ -25,8 +25,53 @@ def crear_dama(x,y,r,g,b,radius):
     
     return numpy.array(circle, dtype = numpy.float32)
 
+# Esta funcion crea un fondo negro, sobre la cual vamos a poner las casillas blancas
+def crear_fondo():
+    fondo = [
+        -0.75, -0.75, 0, 0, 0, 0,
+        0.75, -0.75, 0, 0, 0, 0, 
+        0.75, 0.75, 0, 0, 0, 0,
+        0.75, 0.75, 0, 0, 0, 0,
+        -0.75, 0.75, 0, 0, 0, 0,
+        -0.75, -0.75, 0, 0, 0, 0
+    ]
+    return numpy.array(fondo, dtype=numpy.float32)
+
+
+# crea un cuadrado de dimensiones 0.1875 x 0.1875
+# x e y son las coordenadas del vertice inferior izquierdo del cuadrado
+def crear_cuadrado(x, y, r, g, b):
+    cuadrado = [
+        x, y, 0.0, r, g, b,
+        x + 0.1875, y, 0.0, r, g, b,
+        x + 0.1875, y + 0.1875, 0.0,  r, g, b, 
+        x + 0.1875, y + 0.1875, 0.0, r, g, b, 
+        x, y + 0.1875, 0.0, r, g, b, 
+        x, y, 0.0, r, g, b
+    ]
+    return cuadrado
+
+
+# crea el arreglo de las casillas blancas
+def crear_tablero():
+    output = []
+    for i in range(8):
+        # las filas pares el primer cuadrado blanco empieza en (-0.75, -0.75)
+        if i % 2 == 0:
+            # creamos 4 casillas blancas a una distancia de 0.375 cada una
+            for j in range(4):
+                output.extend(crear_cuadrado(-0.75 + 0.375 * j, -0.75 + 0.1875 * i, 1, 1, 1))
+        # en las filas impares el primer cuadrado blanco empieza en (-0.5625, -0.5625)
+        else:
+            # creamos 4 casillas blancas a una distancia de 0.375 cada una
+            for j in range(4):
+                output.extend(crear_cuadrado(-0.5625 + 0.375 * j, -0.75 + 0.1875 * i, 1, 1, 1))
+    
+    return numpy.array(output, dtype=numpy.float32)
+
 if __name__ == "__main__":
 
+    window = None
     # Initialize glfw
     if not glfw.init():
         glfw.set_window_should_close(window, True)
@@ -42,7 +87,9 @@ if __name__ == "__main__":
 
     glfw.make_context_current(window)
 
-    dama = crear_dama(0.5,0.0, 0.0, 1.0, 0.0, 0.2)
+    fondo = crear_fondo()
+    tablero = crear_tablero()
+    #cuadrado2 = crear_tableros(-0.5625, -0.5625)
 
     # Defining shaders for our pipeline
     vertex_shader = """
@@ -79,10 +126,19 @@ if __name__ == "__main__":
         OpenGL.GL.shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER))
 
     # Each shape must be attached to a Vertex Buffer Object (VBO)
-    vboDama = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, vboDama)
-    glBufferData(GL_ARRAY_BUFFER, len(dama) * SIZE_IN_BYTES, dama, GL_STATIC_DRAW)
+    
+    vboFondo = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, vboFondo)
+    glBufferData(GL_ARRAY_BUFFER, len(fondo) * SIZE_IN_BYTES, fondo, GL_STATIC_DRAW)
 
+    vboTablero = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, vboTablero)
+    glBufferData(GL_ARRAY_BUFFER, len(tablero) * SIZE_IN_BYTES, tablero, GL_STATIC_DRAW)
+
+    """ vboCuadrado2 = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, vboCuadrado2)
+    glBufferData(GL_ARRAY_BUFFER, len(cuadrado2) * SIZE_IN_BYTES, cuadrado2, GL_STATIC_DRAW)
+     """
     # Telling OpenGL to use our shader program
     glUseProgram(shaderProgram)
 
@@ -91,17 +147,27 @@ if __name__ == "__main__":
 
     glClear(GL_COLOR_BUFFER_BIT)
 
-    glBindBuffer(GL_ARRAY_BUFFER, vboDama)
-    position = glGetAttribLocation(shaderProgram, "position")
+    # SETUP DE LA FIGURA 1
+    glBindBuffer(GL_ARRAY_BUFFER, vboFondo)
+    position = glGetAttribLocation(shaderProgram, "position") # Buscar la variable position en el shader, solo es necesario hacer una vez por ShaderProgram
     glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
     glEnableVertexAttribArray(position)
 
-    color = glGetAttribLocation(shaderProgram, "color")
+    color = glGetAttribLocation(shaderProgram, "color") # Buscar la variable color en el shader, solo es necesario hacer una vez por ShaderProgram
     glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
     glEnableVertexAttribArray(color)
-    
-    # It renders a scene using the active shader program (pipeline) and the active VAO (shapes)
-    glDrawArrays(GL_TRIANGLES, 0, int(len(dama)/6))
+    # SE DIBUJA LA FIGURA 1
+    glDrawArrays(GL_TRIANGLES, 0, int(len(fondo)/6))
+
+    # SETUP DE LA FIGURA 3
+    glBindBuffer(GL_ARRAY_BUFFER, vboTablero)
+    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(position)
+
+    glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+    glEnableVertexAttribArray(color)
+    # SE DIBUJA LA FIGURA 3
+    glDrawArrays(GL_TRIANGLES, 0, int(len(tablero)/6))   
 
     # Moving our draw to the active color buffer
     glfw.swap_buffers(window)
